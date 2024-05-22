323e795db2d072bfb9f66e9e47eefd7ce45cddc9
mahbub100 <officialmehbub2@gmail.com>
2024-05-22 23:54:59 +0530

    second commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://flat-dekho.vercel.app/api",
+  baseURL: "https://fletdekho-server.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
